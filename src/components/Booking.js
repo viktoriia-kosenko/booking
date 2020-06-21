c920b425b965f758b8013cd65d35828a30e596fa
Vicky <kosenchiha@gmail.com>
2020-06-21 18:24:22 +0100

    Add form to booking page

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -3,7 +3,51 @@ import React, { useState } from "react";
 // add validation for each field
 // enable user to choose previous date,
 const Booking = () => {
-  return <div>This is booking</div>;
+  return (
+    <div>
+      <h1>This is Booking page</h1>
+      <p>Please fill in this form</p>
+
+      <form>
+        <div>
+          <label htmlFor="firstName">First name</label>
+          <input type="text" placeholder="First Name" id="firstName" />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last name</label>
+          <input type="text" placeholder="Last Name" id="lastName" />
+        </div>
+        <div>
+          <label htmlFor="date">Dining Date</label>
+          <input type="date" placeholder="Dining Date" id="date" />
+        </div>
+        <div>
+          <label htmlFor="persons">Number of Covers</label>
+          <input
+            type="number"
+            id="persons"
+            min="1"
+            max="12"
+            placeholder="1-12"
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone Number</label>
+          <input
+            type="tel"
+            id="phone"
+            pattern="[0-9]{11}"
+            placeholder="11 digits"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="xxxxx@xxx.xx" />
+        </div>
+        <button type="submit">Save booking</button>
+      </form>
+    </div>
+  );
 };
 
 export default Booking;
