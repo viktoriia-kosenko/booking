758500032f7730629dea1e58b3dbd702bdb15821
Vicky <kosenchiha@gmail.com>
2020-06-29 18:29:56 +0100

    Rename LaningPage component to Home

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Booking from "../components/BookingPage";
 import Seating from "../components/SeatingPage";
-import LandingPage from "../components/LandingPage";
+import Home from "../components/Home";
 
 export default () => (
   <Switch>
-    <Route path="/" exact component={LandingPage} />
+    <Route path="/" exact component={Home} />
     <Route path="/booking" component={Booking} />
     <Route path="/seating" component={Seating} />
   </Switch>
