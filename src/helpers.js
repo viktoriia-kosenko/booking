fd9f8190ed6db381025237b6046dc24bd8e9fe42
Vicky <kosenchiha@gmail.com>
2020-06-30 17:10:42 +0100

    Remove code about local storage

--- src/helpers.js
+++ src/helpers.js
@@ -1,32 +1,3 @@
-export const getLocalStorageData = (key) => {
-  return localStorage.getItem(key);
-};
-
-export const parseLocalStorageData = (key) => {
-  return JSON.parse(getLocalStorageData(key));
-};
-
-export const saveToLocalStorage = (key, data) => {
-  return localStorage.setItem(key, JSON.stringify(data));
-};
-
-export const addToLocalStorage = (key, data) => {
-  const existingBookings = parseLocalStorageData("bookings");
-  const bookingN = existingBookings.length + 1;
-  const updatedData = [...existingBookings, { ...data, bookingN }];
-  saveToLocalStorage(key, updatedData);
-};
-
-export const saveStatus = (bookingNumber, status) => {
-  const existingBookings = parseLocalStorageData("bookings");
-  const updatedBookings = updateBooking(
-    existingBookings,
-    bookingNumber,
-    status
-  );
-  saveToLocalStorage("bookings", updatedBookings);
-};
-
 export const updateBooking = (bookings, bookingNumber, status) => {
   return bookings.map((booking) =>
     booking.bookingN === bookingNumber ? { ...booking, status } : booking
