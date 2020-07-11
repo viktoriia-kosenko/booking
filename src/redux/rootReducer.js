ff70b2f66151f3cab48f72e002c5235229319cac
Vicky <kosenchiha@gmail.com>
2020-07-11 14:20:20 +0100

    Add only bookings state to the local storage

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import otherReducer from "./otherReducer";
 import bookingReducer from "./bookingReducer";
 
@@ -13,7 +12,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: "root",
   storage,
-  stateReconciler: autoMergeLevel2,
+  whitelist: ["bookings"],
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
